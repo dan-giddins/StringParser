0cd16c0d3c1675d5a2df3fbf702f902fc75914b3
Dan Giddins <daniel.giddins112@gmail.com>
2020-06-23 16:39:44 +0100

    improve

--- StringParser/Program.cs
+++ StringParser/Program.cs
@@ -1,23 +1,24 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace StringParser
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static async Task Main()
 		{
 			var listOfErrors = new List<string>();
 			var apiResponse = System.IO.File.ReadAllText(@"C:\Users\uizdg\The University of Nottingham\Official RIS Team - Documents\Dan Giddins RIS Work\project_response_with_errors.json");
 			var errorMarker = Regex.Escape("notificationMessages\":{\"_objectMessages\":[{\"code\":3010,\"message\":\"");
-			foreach (var match in Regex.Matches(apiResponse, errorMarker).Cast<Match>().Select(m => m.Index))
-			{
-				var error = apiResponse.Substring(match + errorMarker.Length - 3).Split('"').First();
-				Console.WriteLine(error);
-				listOfErrors.Add(error);
-			}
+			var s = new StringContent(JsonConvert.SerializeObject(
+				Regex.Matches(apiResponse, errorMarker).Select(x =>
+					apiResponse.Substring(x.Index + errorMarker.Length - 3).Split('"').First())));
+			Console.WriteLine(await s.ReadAsStringAsync());
 		}
 	}
 }
